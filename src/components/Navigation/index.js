1d21bec7f6714ed846e05a90758e9491f789e8ca
Etienne Warneck <etienne.warneck@gmail.com>
2020-02-29 12:32:06 -0500

    first

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,1 +1,17 @@
+import React from "react";
+
+function Nav() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+      <a className="navbar-brand" href="/">
+        BandAid
+      </a>
+    </nav>
+  );
+}
+
+
+
 export { default } from "./Navigation";
+
+
