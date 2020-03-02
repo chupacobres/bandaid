ff714b60d61168f8df10d69dd851a6fba6c55fee
Etienne Warneck <etienne.warneck@gmail.com>
2020-03-02 10:38:10 -0500

    new branch Nav

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { Component } from 'react';
 import "./Navigation.css";
+// import BANDAID from "./"; 
 
-const Navigation = props => <div className="Navigation">{props.children}</div>;
 
+class Navigation extends Component {
+    render() {
+        return (
+            <nav>
+          <img className="logo" src={require("./BANDAID.png")} />
+            </nav>
+        );
+    }
+}
 export default Navigation;
