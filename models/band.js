5124a714e3fcc70972e7f0da71501d8ca57c9779
Sam Bock <samantha.bock7@gmail.com>
2020-03-02 20:52:39 -0500

    model added

---
+++ models/band.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const bandSchema = new Schema({
+  title: { type: String, required: true },
+  author: { type: String, required: true },
+  synopsis: String,
+  date: { type: Date, default: Date.now }
+});
+const Band = mongoose.model("Book", bandSchema);
+module.exports = Band;
