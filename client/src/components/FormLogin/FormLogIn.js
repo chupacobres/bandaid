8d4ddce1e36675be85603e24afb0cbe0649ca5bf
Etienne Warneck <etienne.warneck@gmail.com>
2020-03-03 15:06:27 -0500

    modal

--- client/src/components/FormLogin/FormLogIn.js
+++ client/src/components/FormLogin/FormLogIn.js
@@ -81,41 +81,5 @@ class FormLogin extends Component {
       );
     }
   }
-  
-// const FormLogIn = props => 
-
-// <Modal.Dialog>
-//   <Modal.Header closeButton>
-//     <Modal.Title>Modal title</Modal.Title>
-//   </Modal.Header>
-
-//   <Modal.Body>
-//    <Form>
-//   <Form.Group as={Row} controlId="formPlaintextEmail">
-//     <Form.Label column sm="2">
-//       Email
-//     </Form.Label>
-//     <Col sm="10">
-//       <Form.Control defaultValue="email@example.com" />
-//     </Col>
-//   </Form.Group>
-
-//   <Form.Group as={Row} controlId="formPlaintextPassword">
-//     <Form.Label column sm="2">
-//       Password
-//     </Form.Label>
-//     <Col sm="10">
-//       <Form.Control type="password" placeholder="Password" />
-//     </Col>
-//   </Form.Group>
-// </Form>
-//   </Modal.Body>
-
-//   <Modal.Footer>
-//     <Button variant="secondary">Close</Button>
-//     <Button variant="primary">Save changes</Button>
-//   </Modal.Footer>
-// </Modal.Dialog>
-
-
+ 
 export default FormLogIn;
