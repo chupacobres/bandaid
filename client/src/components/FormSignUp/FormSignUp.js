d8056c6db43978374415fa248073aff586248334
Etienne Warneck <etienne.warneck@gmail.com>
2020-03-06 19:45:53 -0500

    form component for SignUp page

--- client/src/components/FormSignUp/FormSignUp.js
+++ client/src/components/FormSignUp/FormSignUp.js
@@ -11,6 +11,13 @@ export class FormSignUp extends Component {
                     <input type="text" id="bname" name="bname"></input>
                     <label for="email">email:</label>
                     <input type="text" id="email" name="email"></input>
+                    <label for="genre">Genre:</label>
+                    <input type="text" id="genre" name="genre"></input>
+                    <label for="lineup">Line-up:</label>
+                    <input type="text" id="lineup" name="lineup"></input>
+                    <label for="location">Location:</label>
+                    <input type="text" id="location" name="location"></input>
+                    {/* IMAGE DOWNLOAD  or YOUTUBE LINK */}
                 </form>
             </div>
         )
