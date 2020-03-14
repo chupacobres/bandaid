a8a2a28ca63f2d313e91c3c7b1c422c7d83612f6
Ben Ng <ben@plumcube.com>
2020-03-13 20:51:15 -0400

    fixed foldername

---
+++ client/src/components/FormLogIn/Modal.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Modal.css';
+import FormLogIn from './FormLogIn';
+
+
+function Modal(props) {
+    return (
+        <div>
+            <FormLogIn
+                close={props.close}
+                handleInputChange={props.handleInputChange}
+                handleFormSubmit={props.handleFormSubmit} />
+        </div>
+    )
+}
+
+export default Modal;
